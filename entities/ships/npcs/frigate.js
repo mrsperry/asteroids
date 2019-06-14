26828eb9d0db8b6a621d447e893acc739eb1e536
Josh <mrjoshuasperry@gmail.com>
2019-06-14 04:36:56 -0400

    update the frigate model and and fix the grapeshot positioning

--- entities/ships/npcs/frigate.js
+++ entities/ships/npcs/frigate.js
@@ -7,6 +7,7 @@ class frigate extends npc {
 
         this.health = 120;
         this.bounds = {
+            position: this.position,
             width: 50,
             height: 20
         };
@@ -37,7 +38,25 @@ class frigate extends npc {
             push();
             translate(this.position.x, this.position.y);
             rotate(this.rotation.heading());
-            rect(0, 0, this.bounds.width, this.bounds.height);
+
+            rectMode(CORNER);
+            // create the frigate shape
+            // rear engines
+            rect(-20, -10, 10, 5);
+            rect(-20, 5, 10, 5);
+            rect(-25, -5, 6, 10);
+            // main body (1px larger than normal to fix a p5 border overlap issue)
+            rect(-20, -8, 41, 16);
+            // front end
+            rect(20, -10, 7, 20);
+            // left side guns
+            circle(-2, -9, 2);
+            circle(4, -9, 2);
+            circle(10, -9, 2);
+            // right side guns
+            circle(-2, 9, 2);
+            circle(4, 9, 2);
+            circle(10, 9, 2);
             pop();
         }
     }
@@ -56,15 +75,33 @@ class frigate extends npc {
 
         // secondary grapeshot
         if (this.secondary_cooldown == 0) {
+            // get the players position
+            let position = ship_manager.player.position.copy();
+            // find the angle between the player and the frigate
+            let angle = atan2(
+                position.y - this.position.y, 
+                position.x - this.position.x);
+            // determine which side to fire from
+            let offset;
+            if (angle >= 45 && angle <= 135) {
+                offset = 12;
+            } else if (angle <= -45 && angle >= -135) {
+                offset = -12;
+            } else {
+                return;
+            }
+            // get the laser heading
+            let heading = position.sub(this.position);
+
             this.secondary_cooldown = round(random(100, 150));
 
-            let position = ship_manager.player.position.copy().sub(this.position);
+            // create the grapeshot
             for (let index = -1; index < 2; index++) {
                 projectile_manager.create_laser(
                     this.faction,
                     laser_type.light,
-                    this.position.copy().add(index * 5), 
-                    position.copy());
+                    this.position.copy().add(4, offset).add(index * 5), 
+                    heading.copy());
             }
         }
 
